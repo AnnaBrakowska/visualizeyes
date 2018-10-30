3781e95624aed8673046d0d570c7ac43570cbf4d
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-30 12:04:29 -0400

    Implemented Chart

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import MainContainer from './containers/MainContainer.jsx';
 import LandingPage from './containers/LandingPage.jsx';
 import FileContainer from './containers/FileContainer.jsx';
+import Tree from 'react-tree-graph';
+
 const beautinator = require("beautinator");
 
 require("./css/style.css");
@@ -26,7 +28,7 @@ class App extends Component {
     fetch('/app/db')
     .then(res => res.json())
     .then ((res) => {
-       console.log('Res', res);
+       console.log('Res >>>>>>', res);
        let keys = Object.keys(res);
        let vals = Object.values(res);
        console.log('keys', keys);
@@ -39,42 +41,74 @@ class App extends Component {
     .catch(err => console.log(err));
   }
 
+
+  componentDidMount() {
+    this.fetchOnClick()
+  }
+
   handleChange(event) {
     this.setState({ url: event.target.value});
   }
 
   render() {
 
-    let documents = [];
-    let objectConstruct = {};
-    let count = 0;
+    // let documents = [];
+    // let objectConstruct = {};
+    // let count = 0;
+    let data = {};
+    
+    data.name = "Things";
+    data.children = this.state.resVals
 
-    for(let i = 0; i < this.state.resKeys.length; i++) {
-      let curKey = this.state.resKeys[i];
-      let curVal = this.state.resVals[i];
+    // for(let i = 0; i < this.state.resVals.length; i++) {
+    //   let curVal = this.state.resVals[i];
+    //   data[i] = curVal;
+    //   console.log('curVal', curVal)
+    //   // count +=1;
+    //   // let document = JSON.stringify(curVal);
+    //   // documents.push(<span className='doc' key={count}><span className="inner">{beautinator(JSON.parse(document))}</span></span>);
 
-      objectConstruct[curKey] = curVal;
-      
-      for (let itm in objectConstruct) {
-        let cur = objectConstruct[itm];
-        console.log('itm', itm);
-        let cleanItm = JSON.parse(JSON.stringify(itm).replace('.', '-').toLowerCase());
-        for (let z = 0; z < cur.length; z++) {
-          count +=1;
-          let document = JSON.stringify(cur[z]);
-          documents.push(<span className={cleanItm} key={count}><span className="inner">{beautinator(JSON.parse(document))}</span></span>);
-        }
-      }
-    }
+    // }
 
+    console.log('data', data);
     return (
-      <div>
-        { this.state.connected ? 
-          <MainContainer /> : 
-          <LandingPage handleChange={ this.handleChange } fetchOnClick={ this.fetchOnClick } url={ this.state.url }/> } 
-          <div className="wrap">{documents}</div>
+      <div className="custom-container">
+        <Tree
+          data={data}
+          width={800}
+          height={480}
+          svgProps={{
+            className: 'custom',
+        }}/>
       </div>
     );
+
+    // for(let i = 0; i < this.state.resVals.length; i++) {
+    //   let curKey = this.state.resKeys[i];
+    //   let curVal = this.state.resVals[i];
+
+    //   objectConstruct[curKey] = curVal;
+      
+    //   for (let itm in objectConstruct) {
+    //     let cur = objectConstruct[itm];
+    //     console.log('itm', itm);
+    //     let cleanItm = JSON.parse(JSON.stringify(itm).replace('.', '-').toLowerCase());
+    //     for (let z = 0; z < cur.length; z++) {
+    //       count +=1;
+    //       let document = JSON.stringify(cur[z]);
+    //       documents.push(<span className={cleanItm} key={count}><span className="inner">{beautinator(JSON.parse(document))}</span></span>);
+    //     }
+    //   }
+    // }
+
+ 
+      // <div>
+      //   { this.state.connected ? 
+      //     <MainContainer /> : 
+      //     <LandingPage handleChange={ this.handleChange } fetchOnClick={ this.fetchOnClick } url={ this.state.url }/> } 
+      //     <div className="wrap">{documents}</div>
+      // </div>
+
  
   }
 }
